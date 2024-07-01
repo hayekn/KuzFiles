687b7ff55482a694f67a500f09b42ae9694b9d5c
Nicholas Hayek <hayekn1@outlook.com>
2024-07-01 15:34:35 -0400

    update

--- Code/Models/pythonizedModel 2.py
+++ Code/Models/pythonizedModel 2.py
@@ -1,21 +1,25 @@
 import numpy as np
-from params import *
+import params
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from scipy.integrate import solve_ivp
 from SALib.analyze import fast, sobol
 from SALib.sample import fast_sampler, saltelli
 
-def F(x):
+def xppaut_model(t, **params.paramDict()):
+
+    #INITIAL CONDITIONS
+    y0 = [0, 0.2, 0.2, 0.3, 0, 0]
+
+    def F(x):
         return 1 / (1 + np.exp(x))
-def xppaut_model(t, y0):
 
     def model(t, y):
         seek, binge, stop, nac, dls, ALCOHOL = y
 
         setp = 1 - F(Esetp * (ALCOHOL - TOLERANCE))
         vta = F(Evta*(ALCOHOL - TOLERANCE*daFACTOR))
-        aps = Eaps*((1-dlsWeight)*nac + dlsWeight*dls)/2
+        aps = Eaps*(nac + dls)/2
 
         dseek_dt = (-seek + F(Eseek * (spTOseek * setp - apsTOseek * aps - seekDRIVE))) / seekTAU
         dbinge_dt = (-binge + F(Ebinge * (stopTObin * stop - seekTObin * seek - bingeDRIVE))) / bingeTAU
@@ -26,28 +30,26 @@ def xppaut_model(t, y0):
 
         return [dseek_dt, dbinge_dt, dstop_dt, dnac_dt, ddls_dt, dALCOHOL_dt]
 
-    sol = solve_ivp(model, (0, t[-1]), y0, dense_output=True)
+    sol = solve_ivp(model, (0, 20), y0, dense_output=True)
     y = sol.sol(t)
-    return {'Int':y, 'Der':model(t,y0)}
+    return y
 
 
 fig, ax = plt.subplots(figsize=(12, 5))
-y0 = [0, 0.2, 0.2, 0.3, 0, 0]
-t = np.linspace(0, 20, 1000)
-y = xppaut_model(t, y0)
-ALCOHOL = y['Int'][5]
+y = xppaut_model(np.linspace(0, 20, 1000))
+ALCOHOL = y[5]
 setp = 1 - F(Esetp * (ALCOHOL - TOLERANCE))
 vta = F(Evta*(ALCOHOL - TOLERANCE*daFACTOR))
 
-ax.plot(t, y['Int'][0], label="seek")
-ax.plot(t, y['Int'][1], label="binge")
-ax.plot(t, y['Int'][2], label="stop")
-ax.plot(t, y['Int'][3], label="nac")
-ax.plot(t, y['Int'][4], label="dls")
+'''ax.plot(t, y[0], label="seek")
+ax.plot(t, y[1], label="binge")
+ax.plot(t, y[2], label="stop")
+ax.plot(t, y[3], label="nac")
+ax.plot(t, y[4], label="dls")
 ax.plot(t, setp, label="setp")
 ax.plot(t,vta, label="vta")
-ax.plot(t, (y['Int'][3]+y['Int'][4])/2, label="avg")
-#ax.plot(t, ALCOHOL, label="ALCOHOL")
-ax.legend()
+ax.plot(t, (y[3]+y[4])/2, label="avg")
+ax.plot(t, ALCOHOL, label="ALCOHOL")
+ax.legend()'''
 
 plt.show()
