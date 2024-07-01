ba2662c46e42b8eb1ef27ac2e746641352430050
Nicholas Hayek <hayekn1@outlook.com>
2024-07-01 14:55:17 -0400

    new

--- Code/Models/pythonizedModel.py
+++ Code/Models/pythonizedModel.py
@@ -1,49 +1,12 @@
 import numpy as np
+import params
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from scipy.integrate import solve_ivp
 from SALib.analyze import fast, sobol
 from SALib.sample import fast_sampler, saltelli
 
-def xppaut_model(t,
-    #EXCITABILITIES
-    Ebinge = 10.5,
-    Estop = 10.5,
-    Enac = 1.84,
-    Eaps = 1,
-    Edls = 1.84,
-    Eseek=1,
-    Esetp = 6,
-    Evta = 12,
-
-    #TIMESCALES
-    seekTAU = 1,
-    bingeTAU = 1,
-    stopTAU = 1,
-    nacTAU = 1,
-    dlsTAU = 1,
-
-    #DRIVES
-    seekDRIVE = 0.01,
-    bingeDRIVE = 0.5,
-    stopDRIVE = 0.5,
-    nacDRIVE = -1.4,
-    dlsDRIVE = -1.4,
-
-    #SYNAPTIC WEIGHTS
-    spTOseek = 5,
-    spTOstop = 1,
-    seekTOnac = 10,
-    seekTObin = 3,
-    binTOnac = 1,
-    binTOstop = 1,
-    binTOdls = 2.5,
-    stopTObin = 2,
-    vtaTOnac = 1,
-    vtaTOdls = 1,
-    apsTOseek = 1,
-    TOLERANCE = 15,
-    daFACTOR = 0.1):
+def xppaut_model(t, **params.paramDict()):
 
     #INITIAL CONDITIONS
     y0 = [0, 0.2, 0.2, 0.3, 0, 0]
@@ -69,27 +32,24 @@ def xppaut_model(t,
 
     sol = solve_ivp(model, (0, 20), y0, dense_output=True)
     y = sol.sol(t)
-
-    fig, ax = plt.subplots(figsize=(12, 5))
-
-    ALCOHOL = y[5]
-    setp = 1 - F(Esetp * (ALCOHOL - TOLERANCE))
-    vta = F(Evta*(ALCOHOL - TOLERANCE*daFACTOR))
-
-    '''ax.plot(t, y[0], label="seek")
-    ax.plot(t, y[1], label="binge")
-    ax.plot(t, y[2], label="stop")
-    ax.plot(t, y[3], label="nac")
-    ax.plot(t, y[4], label="dls")
-    ax.plot(t, setp, label="setp")
-    ax.plot(t,vta, label="vta")
-    ax.plot(t, (y[3]+y[4])/2, label="avg")
-    ax.plot(t, ALCOHOL, label="ALCOHOL")
-    ax.legend()'''
-    ax.plot(t, setp)
-    ax.plot(t, vta)
+    return y
 
 
+fig, ax = plt.subplots(figsize=(12, 5))
+y = xppaut_model(np.linspace(0, 20, 1000))
+ALCOHOL = y[5]
+setp = 1 - F(Esetp * (ALCOHOL - TOLERANCE))
+vta = F(Evta*(ALCOHOL - TOLERANCE*daFACTOR))
 
-xppaut_model(np.linspace(0, 20, 1000))
+'''ax.plot(t, y[0], label="seek")
+ax.plot(t, y[1], label="binge")
+ax.plot(t, y[2], label="stop")
+ax.plot(t, y[3], label="nac")
+ax.plot(t, y[4], label="dls")
+ax.plot(t, setp, label="setp")
+ax.plot(t,vta, label="vta")
+ax.plot(t, (y[3]+y[4])/2, label="avg")
+ax.plot(t, ALCOHOL, label="ALCOHOL")
+ax.legend()'''
+
 plt.show()
--- Sensitivity/sensetivityAnalysis.py
+++ Sensitivity/sensetivityAnalysis.py
@@ -1,55 +1,102 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import odeint
-from scipy.integrate import solve_ivp
+from scipy.integrate import *
 from SALib.analyze import fast, sobol
 from SALib.sample import fast_sampler, saltelli
+keys = {
+    "Ebinge": 0,
+    "Estop": 1,
+    "Enac": 2,
+    "Eaps": 3,
+    "Edls": 4,
+    "Eseek": 5,
+    # TIMESCALES
+    "seekTAU": 6,
+    "bingeTAU": 7,
+    "stopTAU": 8,
+    "nacTAU": 9,
+    # DRIVES
+    "seekDRIVE": 10,
+    "bingeDRIVE": 11,
+    "stopDRIVE": 12,
+    "nacDRIVE": 13,
+    "dlsDRIVE": 14,
+    # SYNAPTIC WEIGHTS
+    "spTOseek": 15,
+    "spSLOPE": 16,
+    "spTOstop": 17,
+    "seekTOnac": 18,
+    "seekTObin": 19,
+    "binTOnac": 20,
+    "binTOstop": 21,
+    "binTOdls": 22,
+    "stopTObin": 23,
+    "vtaTOnac": 24,
+    "vtaTOdls": 25,
+    "apsTOseek": 26,
+    "TOLERANCE": 27,
+    "daFACTOR": 28,
+    "vtaSLOPE": 29,
+    #INITIAL CONDITIONS
+    "seek0":30, 
+    "binge0":31, 
+    "stop0":32,
+    "nac0":33,
+    "dls0":34
+    }
 
-# Define the model function
+typeKeys = {
+        "S1": 0,
+        "ST": 1,
+        "S1_conf": 2,
+        "ST_conf": 3
+    }
+
 def xppaut_model(t,
-    #EXCITABILITIES
-    Ebinge = 10.5,
-    Estop = 10.5,
-    Enac = 1,
-    Eaps = 1,
-    Edls = 1,
-    Eseek=1,
+    Ebinge, 
+    Estop, 
+    Enac, 
+    Eaps, 
+    Edls, 
+    Eseek,
 
     #TIMESCALES
-    seekTAU = 1,
-    bingeTAU = 1,
-    stopTAU = 1,
-    nacTAU = 1,
+    seekTAU, 
+    bingeTAU, 
+    stopTAU, 
+    nacTAU,
 
     #DRIVES
-    seekDRIVE = 0.01,
-    bingeDRIVE = 0.01,
-    stopDRIVE = 0.01,
-    nacDRIVE = -1,
-    dlsDRIVE = 0.01,
+    seekDRIVE, 
+    bingeDRIVE, 
+    stopDRIVE, 
+    nacDRIVE, 
+    dlsDRIVE,
 
     #SYNAPTIC WEIGHTS
-    spTOseek = 5,
-    spSLOPE = 6,
-    seekTOnac = 10,
-    seekTObin = 3,
-    binTOnac = 1,
-    binTOstop = 1,
-    binTOdls = 2.5,
-    stopTObin = 2,
-    vtaTOnac = 1,
-    vtaTOdls = 1,
-    apsTOseek = 1,
-    nsSTART = 0,
-    nsDUR = 0,
-    nsTOstop = 0,
-    nacCAP = 1,
-    TOLERANCE = 5,
-    daFACTOR = 0.25,
-    vtaSLOPE = 4):
+    spTOseek, 
+    spTOstop,
+    spSLOPE, 
+    seekTOnac, 
+    seekTObin, 
+    binTOnac, 
+    binTOstop, 
+    binTOdls, 
+    stopTObin, 
+    vtaTOnac, 
+    vtaTOdls, 
+    apsTOseek, 
 
+    #EXTRA
+    TOLERANCE, 
+    daFACTOR, 
+    vtaSLOPE,
+    
     #INITIAL CONDITIONS
-    y0 = [0, 0.2, 0.2, 0.3, 0, 0]
+    seek0, binge0, stop0, nac0, dls0
+    ):
+
+    y0 = [seek0, binge0, stop0, nac0, dls0, 0]
 
     def F(x):
         return 1 / (1 + np.exp(x))
@@ -63,112 +110,141 @@ def xppaut_model(t,
 
         dseek_dt = (-seek + F(Eseek * (spTOseek * setp - apsTOseek * aps - seekDRIVE))) / seekTAU
         dbinge_dt = (-binge + F(Ebinge * (stopTObin * stop - seekTObin * seek - bingeDRIVE))) / bingeTAU
-        dstop_dt = (-stop + F(Estop * (binTOstop * binge  - stopDRIVE))) / stopTAU
-        dnac_dt = (-nac / nacCAP + F(Enac * (-vtaTOnac * vta - seekTOnac * seek - binTOnac * binge - nacDRIVE))) / nacTAU
+        dstop_dt = (-stop + F(Estop * (binTOstop * binge  - spTOstop * setp - stopDRIVE))) / stopTAU
+        dnac_dt = (-nac + F(Enac * (-vtaTOnac * vta - seekTOnac * seek - binTOnac * binge - nacDRIVE))) / nacTAU
         ddls_dt = (-dls + F(Edls * (-binTOdls * binge - vtaTOdls * vta - dlsDRIVE)))
         dALCOHOL_dt = (dls + nac) / 2
 
         return [dseek_dt, dbinge_dt, dstop_dt, dnac_dt, ddls_dt, dALCOHOL_dt]
 
-    # Solve ODEs
     sol = solve_ivp(model, (0, 20), y0, dense_output=True)
     y = sol.sol(t)
 
-    '''fig, ax = plt.subplots(figsize=(12, 5))
-    ALCOHOL = y[5]
-    setp = 1 - F(spSLOPE * (ALCOHOL - TOLERANCE))
-    ax.plot(t, setp)
-    plt.show()'''
-
-    return [y[-1, t] for t in np.arange(1, 1000, 10)]
-    return [(y[3, t]+y[4, t])/2 for t in np.arange(1, 1000, 10)]
+    return [(y[3, t]+y[4, t])/2 for t in np.arange(1, 1000, 5)]
+    return y
     return (y[3, 50]+y[4, 50])/2
     ALCOHOL_integral = np.trapz(sol[:, 3], t)
     return ALCOHOL_integral
 
-
-'''problem = {
-    'num_vars': 1,
-    'names': ['Test'],
-    'bounds': [[.1, 5]]
-    }
-param_values = fast_sampler.sample(problem, 128)
-t = np.linspace(0, 20, 100)
-Y = np.array([xppaut_model(t, vtaTOdls=sample[0]) for sample in param_values])
-Si = fast.analyze(problem, Y)
-print(Si['S1'])'''
-
-
-def runANALYSIS(steps, range, name):
+def runANALYSIS():
     problem = {
-    'num_vars': 1,
-    'names': ['NA'],
-    'bounds': [range]
+    'num_vars': 35,
+    'names': [
+    #EXCITABILITY
+    'Ebinge','Estop','Enac','Eaps','Edls','Eseek',
+
+    #TIMESCALES'
+    'seekTAU','bingeTAU','stopTAU','nacTAU',
+
+    #DRIVES
+    'seekDRIVE','bingeDRIVE','stopDRIVE','nacDRIVE','dlsDRIVE',
+
+    #SYNAPTIC WEIGHTS
+    'spTOseek','spSLOPE','spTOstop'
+    'seekTOnac','seekTObin',
+    'binTOnac','binTOstop','binTOdls',
+    'stopTObin',
+    'vtaTOnac','vtaTOdls',
+    'apsTOseek',
+
+    #EXTRA
+    'TOLERANCE',
+    'daFACTOR',
+    'vtaSLOPE',
+
+    #INITIAL CONDITIONS
+    'seek0', 'binge0', 'stop0', 'nac0', 'dls0'
+    ],
+
+    'bounds': 
+    [[0, 10]]*6+
+    [[.05, 5]]*4+
+    [[-3, 3]]*5+
+    [[0, 10]]*12+
+    [[.5, 2]]+[[.02, .1]]+[[5, 25]]+
+    [[0, 1]]*5
     }
 
-    param_values = fast_sampler.sample(problem, 128)
+    param_values = fast_sampler.sample(problem, 512)
     t = np.linspace(0, 20, 1000)
 
-    match name:
-        case "spTOseek":
-            Z = np.array([[xppaut_model(t, spTOseek=sample[0])[i] for sample in param_values] for i in np.arange(0, 100, steps)])
-        case "apsTOsp":
-            Z = np.array([[xppaut_model(t, apsTOsp=sample[0])[i] for sample in param_values] for i in np.arange(0, 100, steps)])
-        case "Ebinge":
-            Z = np.array([[xppaut_model(t, Ebinge=sample[0])[i] for sample in param_values] for i in np.arange(0, 100, steps)])
-        case "Estop":
-            Z = np.array([[xppaut_model(t, Estop=sample[0])[i] for sample in param_values] for i in np.arange(0, 100, steps)])
-        case "Eseek":
-            Z = np.array([[xppaut_model(t, Eseek=sample[0])[i] for sample in param_values] for i in np.arange(0, 100, steps)])
-        case "Eaps":
-            Z = np.array([[xppaut_model(t, Eaps=sample[0])[i] for sample in param_values] for i in np.arange(0, 100, steps)])
-        case "Enac":
-            Z = np.array([[xppaut_model(t, Enac=sample[0])[i] for sample in param_values] for i in np.arange(0, 100, steps)])
-        case "Edls":
-            Z = np.array([[xppaut_model(t, Edls=sample[0])[i] for sample in param_values] for i in np.arange(0, 100, steps)])
-        case "vtaTOnac":
-            Z = np.array([[xppaut_model(t, vtaTOnac=sample[0])[i] for sample in param_values] for i in np.arange(0, 100, steps)])
-        case "nacTAU":
-            Z = np.array([[xppaut_model(t, nacTAU=sample[0])[i] for sample in param_values] for i in np.arange(0, 100, steps)])
-        case "seekTAU":
-            Z = np.array([[xppaut_model(t, seekTAU=sample[0])[i] for sample in param_values] for i in np.arange(0, 100, steps)])
-        case "stopTAU":
-            Z = np.array([[xppaut_model(t, stopTAU=sample[0])[i] for sample in param_values] for i in np.arange(0, 100, steps)])
-        case "bingeTAU":
-            Z = np.array([[xppaut_model(t, bingeTAU=sample[0])[i] for sample in param_values] for i in np.arange(0, 100, steps)])
 
-   
-    A = []
-    for z in Z:
-        resultA = z
-        Si = fast.analyze(problem, resultA)
-        A.append(Si['S1'][0])
+    for i in np.arange(0, 200, 1):
+        Y = np.array([xppaut_model(t, *params)[i] for params in param_values])
+        Si = fast.analyze(problem, Y)
+        with open("bigDATAv2.npy", "ab") as f:
+            np.save(f, [Si['S1'], Si['ST'], Si['S1_conf'], Si['ST_conf']])
 
-    n = (np.arange(0, 100, steps))*.2
-    ax.plot(n, A, label=str(name))
+def loadData(names, graph, confidence, file, range=1):
+    with open("bigDATAv2.npy", "rb") as f:
+        A = np.load(f)
 
+    fig, ax = plt.subplots(figsize=(10, 5))
+    n = (np.arange(0, 200, 1))*.1
 
-    plt.ylim(0, 1)
+    for name in names:
+        dat = A[:, typeKeys[graph], (keys[name])]
+        ax.plot(n, dat, label=str(name))
+        if confidence:
+            interval = A[:, typeKeys[graph]+2, (keys[name])]
+            ax.fill_between(n, dat - interval, dat+interval, alpha=0.3)
+
+    plt.legend()
+    plt.ylim(0, range)
     plt.xlim(0, 20)
     plt.xlabel("T")
-    plt.ylabel("Normalized Sensitivity")
+    if graph=='ST':
+        plt.ylabel("Total-Order Sensitivity")
+    else:
+        plt.ylabel("First-Order Sensitivity")
+    #plt.show()
+    plt.savefig(str(file)+".png", dpi=250)
 
 
-fig, ax = plt.subplots(figsize=(12, 5))
-E = ["spTOseek"]
-for e in E:
-    runANALYSIS(2, [0, 12], str(e))
-plt.legend()
-plt.show()
+def chooseBest(n, graph, lookFor):
+    with open("bigDATAv2.npy", "rb") as f:
+        A = np.load(f)
+    result = {}
+    for name in keys:
+        result[name] = simpson(A[:, typeKeys[graph], keys[name]], x=np.linspace(0, 20, 200))
+    
+    names = list(result.keys())
+    values = list(result.values())
+    indexes = np.argsort(values)[-n:]
+    result = {names[i]: values[i] for i in indexes}
+
+    i=0
+    for r in result:
+        if lookFor in r:
+            print(r+": "+str(result[r]))
+            i +=1
+            if i==n:
+                return
+
+
+TAU = ["seekTAU",
+    "bingeTAU",
+    "stopTAU",
+    "nacTAU"]
+E = ["Ebinge",
+    "Estop",
+    "Enac",
+    "Eaps",
+    "Edls",
+    "Eseek"]
+DRIVE = ["seekDRIVE",
+    "bingeDRIVE",
+    "stopDRIVE",
+    "nacDRIVE",
+    "dlsDRIVE"]
+INSULA = ["binTOstop","stopTObin", "Ebinge", "Estop"]
+PFC = ["spTOseek", "apsTOseek", "seekTObin", "spTOstop", "seekTOnac"]
+INIT = ["seek0","binge0","stop0","nac0","dls0"]
+EXTRA = ['TOLERANCE','daFACTOR','vtaSLOPE']
+STR_INP = ['vtaTOnac','vtaTOdls', 'binTOnac','binTOdls','seekTOnac']
 
 
 
-'''
-Y = np.array([xppaut_model(sample[0], t)[-1] for sample in param_values])
-
-# Perform the sensitivity analysis
-Si = fast.analyze(problem, Y)
-
-# Print the results
-print("First-order sensitivity indices:", Si['S1'])
-print("Total-order sensitivity indices:", Si['ST'])'''
+chooseBest(35, "S1", "TO")
+#loadData(["binTOstop", "stopTObin", "seekTObin", "Ebinge", "Estop"], "ST", True, "insula", .3)
+#loadData(PFC, "S1", True, "Sensitivity/total/pfc", .35)
