0724b274f72fc89b230cb705abc37a462ee1675d
Nicholas Hayek <hayekn1@outlook.com>
2024-07-02 09:29:15 -0400

    derModel

--- Code/Models/pythonizedModel.py
+++ Code/Models/pythonizedModel.py
@@ -4,6 +4,24 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from scipy.integrate import solve_ivp
 
+
+
+def derModel(t, y0):
+    seek, binge, stop, nac, dls, ALCOHOL = y
+
+    setp = 1 - F(Esetp * (ALCOHOL - TOLERANCE))
+    vta = F(Evta*(ALCOHOL - TOLERANCE*daFACTOR))
+    aps = Eaps*((1-dlsWeight)*nac + dlsWeight*dls)/2
+
+    dseek_dt = (-seek + F(Eseek * (spTOseek * setp - apsTOseek * aps - seekDRIVE))) / seekTAU
+    dbinge_dt = (-binge + F(Ebinge * (stopTObin * stop - seekTObin * seek - bingeDRIVE))) / bingeTAU
+    dstop_dt = (-stop + F(Estop * (binTOstop * binge - spTOstop * setp - stopDRIVE))) / stopTAU
+    dnac_dt = (-nac + F(Enac * (-vtaTOnac * vta - seekTOnac * seek - binTOnac * binge - nacDRIVE))) / nacTAU
+    ddls_dt = (-dls + F(Edls * (-binTOdls * binge - vtaTOdls * vta - dlsDRIVE))) / dlsTAU
+    dALCOHOL_dt = ((1-dlsWeight)*nac + dlsWeight*dls)/2
+
+    return [dseek_dt, dbinge_dt, dstop_dt, dnac_dt, ddls_dt, dALCOHOL_dt]
+
 def F(x):
         return 1 / (1 + np.exp(x))
 def xppaut_model(t, y0):
@@ -26,7 +44,7 @@ def xppaut_model(t, y0):
 
     sol = solve_ivp(model, (0, t[-1]), y0, dense_output=True)
     y = sol.sol(t)
-    return {'Int':y, 'Der':model(t,y0)}
+    return {'Int':y, 'Der':[model(t,y0) for t in t]}
 
 def runGraphs():
     fig, ax = plt.subplots(figsize=(12, 5))
@@ -107,7 +125,7 @@ def stopBif():
     plt.savefig("stopBif", dpi=350)
 
 
-stopBif()
+'''stopBif()
 bingeBif()
 
 R = np.linspace(0, 1, 100)
@@ -115,4 +133,7 @@ R = np.linspace(0, 1, 100)
 #[insulaBif(binExc=e) for e in np.linspace(0, 10, 100)]
 P = {'binExc':2, 'stopExc':Estop}
 ax.plot(insulaNull(R, **P)[0], R)
-ax.plot(R, insulaNull(R, **P)[1])
+ax.plot(R, insulaNull(R, **P)[1])'''
+
+
+print(xppaut_model(np.linspace(0, 20, 100), [0, 0, 0, 0, 0, 0])['Der'])
