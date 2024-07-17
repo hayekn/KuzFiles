d8558bc65f17ca38bc33843f3233e6530148f8b0
amysrude <amysrude@gmail.com>
2024-07-17 10:01:13 -0400

    updated final graphs

--- Code/Models/FINAL/final.py
+++ Code/Models/FINAL/final.py
@@ -84,36 +84,50 @@ def xppaut_model(t, fuzz, csTOvta=csTOvta):
 
 
 def runGraphs(time=120, fuzz=False, save=False, anim=False):
-    fig, axs = plt.subplots(3, 3, figsize=(11, 8))
+    fig, axs = plt.subplots(2, 3, figsize=(12, 8))
     t = np.linspace(0, time, 300)
     y = xppaut_model(t, fuzz)
     sp = axs[0, 0].plot(t, y['Int'][0], label="Setpoint", color = 'royalblue')[0]
     seek = axs[0, 0].plot(t, y['Int'][1], label="Seek", color = 'midnightblue')[0]
     comb = axs[0, 0].plot(t, (y['Int'][0]+y['Int'][1])/2, '--', label="mPFC Average", color = 'lightblue')[0]
-    axs[0,0].set_title("mPFC")
+    axs[0,0].set_title("mPFC Activity")
+    
     bin = axs[0, 1].plot(t, y['Int'][2], label="Binge", color = 'mediumseagreen')[0]
-    axs[0,1].set_title("Insula")
+    axs[0,1].set_title("Insular Activity")
+    
     nac = axs[0, 2].plot(t, y['Int'][3], label="NAc", color = 'maroon')[0]
     dls = axs[0, 2].plot(t, y['Int'][4], label="DLS", color='red')[0]
     sum = axs[0,2].plot(t, y['Int'][3]+y['Int'][4], '--',label="Striatum", color='tomato')[0]
-    axs[0, 2].set_title("\"Effort\"")
-    da = axs[1, 1].plot(t, y['Int'][5], label="VTA", color = 'lightcoral')[0]
-    axs[1,1].set_title("DA")
-    alc = axs[1, 0].plot(t, y['Int'][6], label='Alcohol Vol.', color = 'red')[0]
-    cond = axs[1, 2].plot(t, cs, label="CS")[0]
-    excNac = axs[1,2].plot(t, y['Int'][7], label="Enac")[0]
-    driNac = axs[2, 0].plot(t, y['Int'][8], label='driveNAC')[0]
-    axs[1,2].set_title("Conditioned/Negative Stimulus")
+    axs[0, 2].set_title("Striatal Activity")
+
+    da = axs[1, 0].plot(t, y['Int'][5], label="VTA", color = 'lightcoral')[0]
+    axs[1,0].set_title("VTA Activity")
+
+    excNac = axs[1,1].plot(t, y['Int'][7], label="$E_{NAc}$")[0]
+    driNac = axs[1, 1].plot(t, abs(y['Int'][8]), label='$drive_{NAc}$')[0]
+    axs[1,1].set_title("DA Modulation of NAc Parameters")
+    
+    alc = axs[1, 2].plot(t, y['Int'][6], label='Alcohol Vol.', color = 'red')[0]
+    axs[1,2].set_title("Alcohol Consumption")
+    for z in range(3):
+         axs[0,z].set_ylabel('Firing Rate (Hz)')
+    axs[1,0].set_ylabel('Firing Rate (Hz)')
+    axs[1,2].set_ylabel('Volume')
     for i in range(2):
          for j in range(3):
-              axs[i, j].legend()
+            axs[i, j].legend()
+            axs[i,j].set_xlabel('Time (mins)')
+            axs[i,j].fill_between(t, 30, where=[(t >= 0) and (t <= 3) for t in t], color = 'grey', alpha = 0.15, linewidth = 0.05) 
+            axs[i,j].set_xlim(0,t[-1])
     axs[0, 0].set_ylim(0, 1)
     axs[0, 1].set_ylim(0, 1)
     axs[0, 2].set_ylim(0, 1.2)
-
+    axs[1, 0].set_ylim(0, 1)
+    axs[1, 1].set_ylim(0, 7.5)
+    axs[1, 2].set_ylim(0, 30)
     frames=100
     def update(frame):
-        y = xppaut_model(t, fuzz, csTOvta=1.5*(frame/frames)+1)
+        y = xppaut_model(t, fuzz, csTOvta=2*(frame/frames)+0)
         sp.set_data(t, y['Int'][0])
         seek.set_data(t, y['Int'][1])
         comb.set_data(t, (y['Int'][0]+y['Int'][1])/2)
@@ -125,14 +139,12 @@ def runGraphs(time=120, fuzz=False, save=False, anim=False):
         alc.set_data(t, y['Int'][6])
         excNac.set_data(t, y['Int'][7])
         driNac.set_data(t, y['Int'][8])
-
-
         return (sp, seek, nac)
     if anim:
-        ani = animation.FuncAnimation(fig=fig, func=update, frames=frames, interval=10)
+        ani = animation.FuncAnimation(fig=fig, func=update, frames=frames, interval=10, repeat = False)
     plt.tight_layout()
     plt.show()
 
 
-#runGraphs(120)
+runGraphs(120)
 
