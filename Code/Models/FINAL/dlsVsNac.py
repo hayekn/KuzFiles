857f916ff29a9f5d0577f80f95fd439964873be7
Nicholas Hayek <hayekn1@outlook.com>
2024-07-17 10:01:22 -0400

    sync

--- Code/Models/FINAL/dlsVsNac.py
+++ Code/Models/FINAL/dlsVsNac.py
@@ -4,11 +4,13 @@ nac = xppaut_model(t, False)['Int'][3]
 dls = xppaut_model(t, False)['Int'][4]
 
 result = dls/(dls+nac)
-nacResult
-fig, ax = plt.subplots(figsize=(12, 8))
-ax.plot(t, result)
-ax.set_ylabel("DLS Contribution To Consumption")
+
+fig, ax = plt.subplots(figsize=(12, 5))
+ax.plot(t, result, label="DLS")
+ax.plot(t, 1-result, label="NAc")
+plt.title("Striatal Contribution To Consumption")
 ax.set_xlabel("T (min)")
+plt.legend()
 plt.show()
 
 
