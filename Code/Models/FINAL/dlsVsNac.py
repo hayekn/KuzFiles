cd06de376804ceb320f8cbecedfcbc041e022d97
Nicholas Hayek <hayekn1@outlook.com>
2024-07-20 14:53:53 -0400

    sync

--- Code/Models/FINAL/dlsVsNac.py
+++ Code/Models/FINAL/dlsVsNac.py
@@ -9,16 +9,35 @@ t = np.linspace(0, 120, 300)
 nac = xppaut_model(t, False)['Int'][3]
 dls = xppaut_model(t, False)['Int'][4]
 
-result = dls/(dls+nac)
-fig, ax = plt.subplots(figsize=(8, 8))
-ax.plot(t, result, label="DLS")
+result = 100*(dls/(dls+nac))
+fig, ax = plt.subplots(1, 2, sharey=True, facecolor='w', gridspec_kw={'width_ratios': [10, 1]}, figsize=(8, 8))
+for i in range(2):
+    ax[i].plot(t, result, label="DLS")
+    ax[i].plot(t, 100-result, label="NAc")
 
-plt.xticks(fontsize=24)
-plt.yticks(fontsize=24)
+plt.xticks(fontsize=22)
+plt.yticks(fontsize=22)
 
-ax.plot(t, 1-result, label="NAc")
-ax.fill_between(t, 1, where=[(t >= 0) and (t <= 3) for t in t], color = 'red', alpha = 0.15, linewidth = 0.05, label='CS') 
-ax.set_xlabel("T (min)")
+ax[0].set_xlim(0, 40)
+ax[1].set_xlim(115, 120)
+ax[1].set_xticks([120])
+ax[0].spines['right'].set_visible(False)
+ax[1].spines['left'].set_visible(False)
+ax[1].tick_params(left = False) 
+d = .015  
+r = 0
+s = 0.15
+kwargs = dict(transform=ax[0].transAxes, color='k', clip_on=False)
+ax[0].plot((1-d+r, 1+d+r), (-d, +d), **kwargs)
+ax[0].plot((1-d+r, 1+d+r), (1-d, 1+d), **kwargs)
+kwargs.update(transform=ax[1].transAxes)  # switch to the bottom axes
+ax[1].plot((-d-0.3+s, +d+s), (1-d, 1+d), **kwargs)
+ax[1].plot((-d-0.3+s, +d+s), (-d, +d), **kwargs)
+ax[0].set_xlabel('Time (min)')
+ax[0].set_ylabel('Striatum Load (\%)')
+ax[0].xaxis.set_label_coords(0.6, -0.09)
+
+ax[0].fill_between(t, 100, where=[(t >= 0) and (t <= 3) for t in t], color = 'red', alpha = 0.15, linewidth = 0.05, label='CS') 
 plt.legend()
 plt.savefig("Code/Models/FINAL/Pics/dlsFig.png", transparent=False, dpi=400)
 
